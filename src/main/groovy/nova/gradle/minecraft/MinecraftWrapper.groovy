9cadff9f80d2839cf9375a6085b24aba2717272e
RX14 <chris@rx14.co.uk>
2015-06-11 01:19:35 +0100

    Launching!

--- src/main/groovy/nova/gradle/minecraft/MinecraftWrapper.groovy
+++ src/main/groovy/nova/gradle/minecraft/MinecraftWrapper.groovy
@@ -1,11 +1,15 @@
 package nova.gradle.minecraft
 
-import groovy.transform.CompileStatic
 import nova.gradle.Locality
 import nova.gradle.Wrapper
 import org.gradle.api.Project
+import org.gradle.api.tasks.JavaExec
+import uk.co.rx14.jmclaunchlib.MCInstance
+import uk.co.rx14.jmclaunchlib.util.NullSupplier
 
-@CompileStatic
+import java.nio.file.FileSystems
+import java.nio.file.Path
+
 class MinecraftWrapper implements Wrapper {
 	@Override
 	String getName() {
@@ -14,6 +18,26 @@ class MinecraftWrapper implements Wrapper {
 
 	@Override
 	void addTask(Project project, String taskName, Locality locality, Map<String, String> options) {
-		//
+		project.task(type: JavaExec, taskName).doFirst {
+			project.logger.lifecycle "Creating instance..."
+			def instancePath = FileSystems.default.getPath("run/MC/client")
+
+			instancePath.toFile().mkdirs()
+
+			def instance = MCInstance.createForge(
+				"1.7.10",
+				"1.7.10-10.13.2.1300-1.7.10",
+				instancePath,
+				project.gradle.gradleUserHomeDir.toPath().resolve("caches/minecraft"),
+				NullSupplier.INSTANCE
+			)
+
+			def spec = instance.getOfflineLaunchSpec("TestUser-${new Random().nextInt(100)}")
+
+			classpath = project.files(spec.classpath)
+			args = spec.launchArgs.toList()
+			main = spec.mainClass
+			jvmArgs = spec.jvmArgs.toList()
+		}
 	}
 }
